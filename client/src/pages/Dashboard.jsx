2d7979b6393808a06cbae10171965f6529d98d64
Carol Wargo <Carolwargo@gmail.com>
2024-08-03 16:46:17 -0400

    performed installs

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/Dashboard/DashSidebar';
+import DashProfile from '../components/Dashboard/DashProfile';
 //import DashPosts from '../components/DashPosts';
-import DashUsers from '../components/DashUsers';
+import DashUsers from '../components/Dashboard/DashUsers';
 //import DashComments from '../components/DashComments';
-import DashboardComp from '../components/DashboardComp';
+import DashboardComp from '../components/Dashboard/DashboardComp';
 
 
 function Dashboard() {
